559f80d687688a29d482c3ee3355e943e2c6e2eb
QUANON <quanon86@gmail.com>
2021-03-15 20:38:58 +0900

    Set global style

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import App from 'next/app';
-
+import App, { Container } from 'next/app';
 import '../.semantic/dist/semantic.min.css';
 
-class MyApp extends App {
+export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
-    return <Component {...pageProps} />;
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+        <style jsx global>{`
+          body {
+            padding: 1rem;
+          }
+        `}</style>
+      </Container>
+    );
   }
 }
-
-export default MyApp;
