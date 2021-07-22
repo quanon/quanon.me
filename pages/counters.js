2507a2bfc56be9ebd5978556d0eff9fb358a30d4
QUANON <quanon86@gmail.com>
2021-07-22 16:04:24 +0900

    Make MTG Life counters a PWA

--- pages/counters.js
+++ pages/counters.js
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import styled from 'styled-components';
 import Counter from '../components/Counter';
 import ResetButton from '../components/ResetButton';
+import Head from 'next/head';
 
 const UpsideDownCounter = styled(Counter)`
   transform: scaleX(-1) scaleY(-1);
@@ -12,27 +13,34 @@ const Counters = () => {
   const opponentRef = useRef();
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-      }}>
-      <UpsideDownCounter
-        ref={opponentRef}
-        colors={{ card: '#73b1e2', button: '#5ea5de', history: '#2981c6' }}></UpsideDownCounter>
-      <ResetButton
-        onClick={() => {
-          if (window.confirm('Do you really want to reset the life counters?')) {
-            myRef.current.reset();
-            opponentRef.current.reset();
-          }
-        }}></ResetButton>
-      <Counter
-        ref={myRef}
-        colors={{ card: '#e273b1', button: '#de5ea5', history: '#c62981' }}></Counter>
-    </div>
+    <>
+      <Head>
+        <title>MTG Life Counters</title>
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <link rel="apple-touch-icon" href="/icon-192x192.png" />
+      </Head>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column'
+        }}>
+        <UpsideDownCounter
+          ref={opponentRef}
+          colors={{ card: '#73b1e2', button: '#5ea5de', history: '#2981c6' }}></UpsideDownCounter>
+        <ResetButton
+          onClick={() => {
+            if (window.confirm('Do you really want to reset the life counters?')) {
+              myRef.current.reset();
+              opponentRef.current.reset();
+            }
+          }}></ResetButton>
+        <Counter
+          ref={myRef}
+          colors={{ card: '#e273b1', button: '#de5ea5', history: '#c62981' }}></Counter>
+      </div>
+    </>
   );
 };
 
